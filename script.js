81230452cfba41d8a070cf16802b09694b786f22
Muhammed Humraz <muhammadhamraz201@gmail.com>
2025-04-20 12:04:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+function slideLeft() {
+  document.getElementById("productSlider").scrollBy({
+    left: -300,
+    behavior: "smooth"
+  });
+}
+
+function slideRight() {
+  document.getElementById("productSlider").scrollBy({
+    left: 300,
+    behavior: "smooth"
+  });
+}
+
+
 function slideLeft(sliderId) {
   const slider = document.getElementById(sliderId);
   slider.scrollBy({ left: -300, behavior: 'smooth' });
