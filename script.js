20ab27da28cd22e2b99f73e7d5cd12a0df8122ed
Muhammed Humraz <muhammadhamraz201@gmail.com>
2025-04-21 17:10:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+
+function updatePrice(selectElement) {
+  const price = selectElement.value;
+  const priceElement = selectElement.parentElement.querySelector('.price');
+  priceElement.textContent = `Rs. ${price}`;
+}
+
+function addToCartWithSize(button, productName) {
+  const select = button.parentElement.querySelector('select');
+  const selectedPrice = select.value;
+  const selectedSize = select.options[select.selectedIndex].text.split(" - ")[0];
+  alert(`${productName} (${selectedSize}) added to cart for Rs. ${selectedPrice}`);
+  // You can customize this function to push to a cart array
+}
+
+
+
+
+
 function slideLeft() {
   document.getElementById("productSlider").scrollBy({
     left: -300,
@@ -22,17 +41,4 @@ function slideRight(sliderId) {
   const slider = document.getElementById(sliderId);
   slider.scrollBy({ left: 300, behavior: 'smooth' });
 }
-
-function updatePrice(selectElement) {
-  const price = selectElement.value;
-  const priceElement = selectElement.parentElement.querySelector('.price');
-  priceElement.textContent = `Rs. ${price}`;
-}
-
-function addToCartWithSize(button, productName) {
-  const select = button.parentElement.querySelector('select');
-  const selectedPrice = select.value;
-  const selectedSize = select.options[select.selectedIndex].text.split(" - ")[0];
-  alert(`${productName} (${selectedSize}) added to cart for Rs. ${selectedPrice}`);
-  // You can customize this function to push to a cart array
-}
+ 
