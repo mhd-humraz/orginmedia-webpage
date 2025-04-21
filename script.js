ed97a48f5bc1bac841f89a04a95cc9fec0e014df
Muhammed Humraz <muhammadhamraz201@gmail.com>
2025-04-21 17:02:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,17 @@ function slideRight(sliderId) {
   const slider = document.getElementById(sliderId);
   slider.scrollBy({ left: 300, behavior: 'smooth' });
 }
+
+function updatePrice(selectElement) {
+  const price = selectElement.value;
+  const priceElement = selectElement.parentElement.querySelector('.price');
+  priceElement.textContent = `Rs. ${price}`;
+}
+
+function addToCartWithSize(button, productName) {
+  const select = button.parentElement.querySelector('select');
+  const selectedPrice = select.value;
+  const selectedSize = select.options[select.selectedIndex].text.split(" - ")[0];
+  alert(`${productName} (${selectedSize}) added to cart for Rs. ${selectedPrice}`);
+  // You can customize this function to push to a cart array
+}
