d08a7b094691f4b5be31fd64ec63b34cb371bd62
Muhammed Humraz <muhammadhamraz201@gmail.com>
2025-05-26 21:01:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -41,4 +41,11 @@ function slideRight(sliderId) {
   const slider = document.getElementById(sliderId);
   slider.scrollBy({ left: 300, behavior: 'smooth' });
 }
+let cart = JSON.parse(localStorage.getItem("cartItems")) || [];
+cart.push({
+  name: "Poster Name",
+  price: 100,
+  image: "images/poster.jpg"
+});
+localStorage.setItem("cartItems", JSON.stringify(cart));
  
